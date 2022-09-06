66ae9a7cbe672ed88c08fad21d3b349502916e41
zombified_potato <bryanbelle2003@gmail.com>
2022-09-06 11:01:35 +0200

    Moved lightning effect from extra_origin_problems.

--- src/main/java/com/github/zombifiedpotato/fantastical_additions/Main.java
+++ src/main/java/com/github/zombifiedpotato/fantastical_additions/Main.java
@@ -1,5 +1,6 @@
-package com.github.ZombifiedPatato.fantastical_additions;
+package com.github.zombifiedpotato.fantastical_additions;
 
+import com.github.zombifiedpotato.fantastical_additions.effect.ModEffects;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +17,7 @@ public class Main implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("fantastical_additions is starting! Are you ready for magic?");
+		ModEffects.registerEffects();
 	}
 }
